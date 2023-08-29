d492bc6197ce9503562a2e499cf6a629ce9b6ad0
deviantony <anthony.lapenna@portainer.io>
2023-08-29 19:04:35 +0000

    feat: implement namespace support in memory store

--- internal/adapter/store/memory/secret.go
+++ internal/adapter/store/memory/secret.go
@@ -2,6 +2,8 @@ package memory
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"sync"
 
 	storeerr "github.com/portainer/k2d/internal/adapter/store/errors"
@@ -25,7 +27,17 @@ type InMemoryStore struct {
 	secretMap map[string]secretData
 }
 
+func buildSecretKey(secretName, namespace string) string {
+	return fmt.Sprintf("%s-%s", namespace, secretName)
+}
+
+func getSecretNameFromKey(key, namespace string) string {
+	return strings.TrimPrefix(key, fmt.Sprintf("%s-", namespace))
+}
+
 // NewInMemoryStore creates a new in-memory store
+// Secrets are stored in a map with the key using a specific format:
+// <namespace>-<secretName>
 func NewInMemoryStore() *InMemoryStore {
 	return &InMemoryStore{
 		m:         sync.RWMutex{},
@@ -37,7 +49,7 @@ func NewInMemoryStore() *InMemoryStore {
 func (s *InMemoryStore) DeleteSecret(secretName, namespace string) error {
 	s.m.Lock()
 	defer s.m.Unlock()
-	delete(s.secretMap, secretName)
+	delete(s.secretMap, buildSecretKey(secretName, namespace))
 	return nil
 }
 
@@ -50,7 +62,7 @@ func (s *InMemoryStore) GetSecretBinds(secret *core.Secret) (map[string]string, 
 func (s *InMemoryStore) GetSecret(secretName, namespace string) (*core.Secret, error) {
 	s.m.RLock()
 	defer s.m.RUnlock()
-	data, found := s.secretMap[secretName]
+	data, found := s.secretMap[buildSecretKey(secretName, namespace)]
 	if !found {
 		return nil, storeerr.ErrResourceNotFound
 	}
@@ -63,7 +75,7 @@ func (s *InMemoryStore) GetSecret(secretName, namespace string) (*core.Secret, e
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        secretName,
 			Annotations: map[string]string{},
-			Namespace:   "default",
+			Namespace:   namespace,
 		},
 		Data: data.Data,
 		Type: core.SecretType(data.Type),
@@ -76,7 +88,11 @@ func (s *InMemoryStore) GetSecrets(namespace string, selector labels.Selector) (
 	defer s.m.RUnlock()
 	var secrets []core.Secret
 
-	for name, data := range s.secretMap {
+	for key, data := range s.secretMap {
+
+		if !strings.HasPrefix(key, namespace) {
+			continue
+		}
 
 		secret := core.Secret{
 			TypeMeta: metav1.TypeMeta{
@@ -84,9 +100,9 @@ func (s *InMemoryStore) GetSecrets(namespace string, selector labels.Selector) (
 				APIVersion: "v1",
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:        name,
+				Name:        getSecretNameFromKey(key, namespace),
 				Annotations: map[string]string{},
-				Namespace:   "default",
+				Namespace:   namespace,
 			},
 			Data: data.Data,
 			Type: core.SecretType(data.Type),
@@ -105,7 +121,7 @@ func (s *InMemoryStore) StoreSecret(secret *corev1.Secret) error {
 	s.m.Lock()
 	defer s.m.Unlock()
 
-	s.secretMap[secret.Name] = secretData{
+	s.secretMap[buildSecretKey(secret.Name, secret.Namespace)] = secretData{
 		Data: secret.Data,
 		Type: string(secret.Type),
 	}
