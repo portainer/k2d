56b1ff2c0944a83a166b726c15d69f4ac3f9c96b
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2023-09-04 16:09:12 +0200

    fix(batch/job): fixed issue with invalid version
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- internal/adapter/container_utils.go
+++ internal/adapter/container_utils.go
@@ -24,6 +24,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
+	"k8s.io/kubernetes/pkg/apis/batch"
 	"k8s.io/kubernetes/pkg/apis/core"
 )
 
@@ -310,7 +311,81 @@ func (adapter *KubeDockerAdapter) createContainerFromPodSpec(ctx context.Context
 // createContainerFromJobSpec orchestrates the creation of a Docker container based on a given Kubernetes PodSpec.
 // TODO: needs full description
 func (adapter *KubeDockerAdapter) createContainerFromJobSpec(ctx context.Context, options ContainerCreationOptions) error {
-	panic("unimplemented")
+	if options.lastAppliedConfiguration != "" {
+		options.labels[k2dtypes.WorkloadLastAppliedConfigLabelKey] = options.lastAppliedConfiguration
+	}
+
+	internalJobSpec := batch.JobSpec{}
+	err := adapter.ConvertK8SResource(&options.jobSpec, &internalJobSpec)
+	if err != nil {
+		return fmt.Errorf("unable to convert versioned pod spec to internal pod spec: %w", err)
+	}
+
+	internalJobSpecData, err := json.Marshal(internalJobSpec)
+	if err != nil {
+		return fmt.Errorf("unable to marshal internal pod spec: %w", err)
+	}
+	options.labels[k2dtypes.PodLastAppliedConfigLabelKey] = string(internalJobSpecData)
+	options.labels[k2dtypes.NamespaceLabelKey] = options.namespace
+	options.labels[k2dtypes.WorkloadNameLabelKey] = options.containerName
+	options.labels[k2dtypes.NetworkNameLabelKey] = buildNetworkName(options.namespace)
+
+	containerCfg, err := adapter.converter.ConvertJobSpecToContainerConfiguration(internalJobSpec, options.namespace, options.labels)
+	if err != nil {
+		return fmt.Errorf("unable to build container configuration from pod spec: %w", err)
+	}
+	containerCfg.ContainerName = buildContainerName(options.containerName, options.namespace)
+
+	existingContainer, err := adapter.getContainer(ctx, containerCfg.ContainerName)
+	if err != nil {
+		return fmt.Errorf("unable to inspect container: %w", err)
+	}
+
+	if existingContainer != nil {
+		if options.lastAppliedConfiguration == existingContainer.Config.Labels[k2dtypes.WorkloadLastAppliedConfigLabelKey] {
+			adapter.logger.Infof("container with the name %s already exists with the same configuration. The update will be skipped", containerCfg.ContainerName)
+			return nil
+		}
+
+		adapter.logger.Infof("container with the name %s already exists with a different configuration. The container will be recreated", containerCfg.ContainerName)
+
+		if existingContainer.Config.Labels[k2dtypes.ServiceLastAppliedConfigLabelKey] != "" {
+			options.labels[k2dtypes.ServiceLastAppliedConfigLabelKey] = existingContainer.Config.Labels[k2dtypes.ServiceLastAppliedConfigLabelKey]
+		}
+
+		err := adapter.cli.ContainerRemove(ctx, existingContainer.ID, types.ContainerRemoveOptions{Force: true})
+		if err != nil {
+			return fmt.Errorf("unable to remove container: %w", err)
+		}
+	}
+
+	registryAuth, err := adapter.getRegistryCredentials(options.podSpec, options.namespace, containerCfg.ContainerConfig.Image)
+	if err != nil {
+		return fmt.Errorf("unable to get registry credentials: %w", err)
+	}
+
+	out, err := adapter.cli.ImagePull(ctx, containerCfg.ContainerConfig.Image, types.ImagePullOptions{
+		RegistryAuth: registryAuth,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to pull %s image: %w", containerCfg.ContainerConfig.Image, err)
+	}
+	defer out.Close()
+
+	io.Copy(os.Stdout, out)
+
+	containerCreateResponse, err := adapter.cli.ContainerCreate(ctx,
+		containerCfg.ContainerConfig,
+		containerCfg.HostConfig,
+		containerCfg.NetworkConfig,
+		nil,
+		containerCfg.ContainerName,
+	)
+	if err != nil {
+		return fmt.Errorf("unable to create container: %w", err)
+	}
+
+	return adapter.cli.ContainerStart(ctx, containerCreateResponse.ID, types.ContainerStartOptions{})
 }
 
 // DeleteContainer attempts to remove a Docker container based on its name and associated namespace.
